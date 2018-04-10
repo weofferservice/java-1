da8f4ddbb4cbd4aaecfc45389f373992e3048b54
weofferservice <weofferservice@hotmail.com>
2018-04-10 20:56:43 +0300

    Добавление интерфейса Storage

---
+++ src/org/zcorp/java1/storage/Storage.java
@@ -0,0 +1,21 @@
+package org.zcorp.java1.storage;
+
+import org.zcorp.java1.model.Resume;
+
+public interface Storage {
+
+    void clear();
+
+    void update(Resume r);
+
+    void save(Resume r);
+
+    Resume get(String uuid);
+
+    void delete(String uuid);
+
+    Resume[] getAll();
+
+    int size();
+
+}
