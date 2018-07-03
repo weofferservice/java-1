a06539f5fdb8f1332c60a4d48797e28cfb34f2f3
weofferservice <weofferservice@hotmail.com>
2018-07-03 18:52:59 +0300

    Объединение тестов в Suite-класс

---
+++ test/org/zcorp/java1/storage/AllStorageTest.java
@@ -0,0 +1,17 @@
+package org.zcorp.java1.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses(
+        {
+                ArrayStorageTest.class,
+                SortedArrayStorageTest.class,
+                ListStorageTest.class,
+                MapUuidStorageTest.class,
+        }
+)
+public class AllStorageTest {
+
+}
