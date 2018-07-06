19c62d8db854d2ba9bac116d493fa7d2eb0a8dc5
weofferservice <weofferservice@hotmail.com>
2018-07-06 10:51:20 +0300

    Решение 8.1 Переделать модель резюме: учесть, что на одной работе (и/или в одном учебном заведении) можно работать/учиться в разные периоды и при этом имя организации не дублируется

--- src/org/zcorp/java1/model/Organization.java
+++ src/org/zcorp/java1/model/Organization.java
@@ -1,25 +1,64 @@
 package org.zcorp.java1.model;
 
 import java.time.LocalDate;
-import java.util.Objects;
+import java.util.*;
 
 public class Organization {
     private final Link homePage;
+    private final List<Period> periods = new ArrayList<>();
 
-    private final LocalDate startDate;
-    private final LocalDate endDate;
-    private final String title;
-    private final String description;
+    public Organization(String name, String url) {
+        this.homePage = new Link(name, url);
+    }
 
     public Organization(String name, String url, LocalDate startDate, LocalDate endDate, String title, String description) {
-        Objects.requireNonNull(startDate, "startDate must not be null");
-        Objects.requireNonNull(endDate, "endDate must not be null");
-        Objects.requireNonNull(title, "title must not be null");
-        this.homePage = new Link(name, url);
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.title = title;
-        this.description = description;
+        this(name, url);
+        periods.add(new Period(startDate, endDate, title, description));
+    }
+
+    public Organization(String name, String url, Period... periods) {
+        this(name, url);
+        this.periods.addAll(Arrays.asList(periods));
+    }
+
+    public Link getHomePage() {
+        return homePage;
+    }
+
+    public List<Period> getPeriods() {
+        return periods;
+    }
+
+    public Period getPeriod(int index) {
+        return periods.get(index);
+    }
+
+    public boolean removePeriod(LocalDate startDate, LocalDate endDate, String title, String description) {
+        return periods.remove(new Period(startDate, endDate, title, description));
+    }
+
+    public boolean removePeriod(Period period) {
+        return periods.remove(period);
+    }
+
+    public Period removePeriod(int index) {
+        return periods.remove(index);
+    }
+
+    public Period setPeriod(int index, LocalDate startDate, LocalDate endDate, String title, String description) {
+        return periods.set(index, new Period(startDate, endDate, title, description));
+    }
+
+    public Period setPeriod(int index, Period period) {
+        return periods.set(index, period);
+    }
+
+    public boolean addPeriod(LocalDate startDate, LocalDate endDate, String title, String description) {
+        return periods.add(new Period(startDate, endDate, title, description));
+    }
+
+    public boolean addPeriod(Period period) {
+        return periods.add(period);
     }
 
     @Override
@@ -30,20 +69,13 @@ public class Organization {
         Organization that = (Organization) o;
 
         if (!homePage.equals(that.homePage)) return false;
-        if (!startDate.equals(that.startDate)) return false;
-        if (!endDate.equals(that.endDate)) return false;
-        if (!title.equals(that.title)) return false;
-        return description != null ? description.equals(that.description) : that.description == null;
-
+        return periods.equals(that.periods);
     }
 
     @Override
     public int hashCode() {
         int result = homePage.hashCode();
-        result = 31 * result + startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        result = 31 * result + title.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + periods.hashCode();
         return result;
     }
 
@@ -51,10 +83,7 @@ public class Organization {
     public String toString() {
         return "Organization{" +
                 "homePage=" + homePage +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
+                ", periods=" + periods +
                 '}';
     }
 }
---
+++ src/org/zcorp/java1/model/Period.java
@@ -0,0 +1,69 @@
+package org.zcorp.java1.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Period {
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+    private final String title;
+    private final String description;
+
+    public Period(LocalDate startDate, LocalDate endDate, String title, String description) {
+        Objects.requireNonNull(startDate, "startDate must not be null");
+        Objects.requireNonNull(endDate, "endDate must not be null");
+        Objects.requireNonNull(title, "title must not be null");
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.title = title;
+        this.description = description;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Period period = (Period) o;
+
+        if (!startDate.equals(period.startDate)) return false;
+        if (!endDate.equals(period.endDate)) return false;
+        if (!title.equals(period.title)) return false;
+        return description != null ? description.equals(period.description) : period.description == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = startDate.hashCode();
+        result = 31 * result + endDate.hashCode();
+        result = 31 * result + title.hashCode();
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Period{" +
+                "startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
