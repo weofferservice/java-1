a00578d1bb5d90e8871803488c66777506394418
weofferservice <weofferservice@hotmail.com>
2018-07-07 13:49:43 +0300

    Создание теста для хранилища резюме в файлах на основе стандартной сериализации Java-объектов

--- test/org/zcorp/java1/storage/AbstractStorageTest.java
+++ test/org/zcorp/java1/storage/AbstractStorageTest.java
@@ -7,6 +7,7 @@ import org.zcorp.java1.exception.ExistStorageException;
 import org.zcorp.java1.exception.NotExistStorageException;
 import org.zcorp.java1.model.*;
 
+import java.io.File;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
@@ -15,6 +16,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public abstract class AbstractStorageTest {
+    protected static final File STORAGE_DIR = new File(".\\storage");
+
     protected Storage storage;
 
     private static final String UUID_1 = "uuid1";
@@ -85,7 +88,7 @@ public abstract class AbstractStorageTest {
     public void update() throws Exception {
         Resume newResume = new Resume(UUID_1, "New Name");
         storage.update(newResume);
-        assertTrue(newResume == storage.get(UUID_1));
+        assertTrue(newResume.equals(storage.get(UUID_1)));
     }
 
     @Test(expected = NotExistStorageException.class)
