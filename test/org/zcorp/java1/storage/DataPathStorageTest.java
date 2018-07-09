4f495c10d918f7077784f0d63eabcebf341cff5e
weofferservice <weofferservice@hotmail.com>
2018-07-09 16:27:19 +0300

    Задание 10. Закончить и протестировать реализацию DataStreamSerializer

---
+++ test/org/zcorp/java1/storage/DataPathStorageTest.java
@@ -0,0 +1,11 @@
+package org.zcorp.java1.storage;
+
+import org.zcorp.java1.storage.serializer.DataStreamSerializer;
+
+public class DataPathStorageTest extends AbstractStorageTest {
+
+    public DataPathStorageTest() {
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new DataStreamSerializer()));
+    }
+
+}
