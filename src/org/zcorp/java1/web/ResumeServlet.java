aa2a4ff2040b017b773999959794aba37a6072cf
weofferservice <weofferservice@hotmail.com>
2018-07-17 15:29:26 +0300

    Создаем сервлет для работы с резюме и настраиваем маппинг на него

---
+++ src/org/zcorp/java1/web/ResumeServlet.java
@@ -0,0 +1,23 @@
+package org.zcorp.java1.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResumeServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        String name = request.getParameter("name");
+        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+    }
+}
