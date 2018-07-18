e6606a462e8c8e0b7e0cb94902dc27d1a0f15379
weofferservice <weofferservice@hotmail.com>
2018-07-19 00:55:48 +0300

    Создаем JSP-страницу и форвордим запрос из сервлета в этот JSP

--- src/org/zcorp/java1/web/ResumeServlet.java
+++ src/org/zcorp/java1/web/ResumeServlet.java
@@ -1,18 +1,15 @@
 package org.zcorp.java1.web;
 
 import org.zcorp.java1.Config;
-import org.zcorp.java1.model.ContactType;
-import org.zcorp.java1.model.Resume;
 import org.zcorp.java1.storage.Storage;
 
 import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
 
 import java.io.IOException;
-import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
 
@@ -24,40 +21,15 @@ public class ResumeServlet extends HttpServlet {
         storage = Config.get().getStorage();
     }
 
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        Writer writer = response.getWriter();
-        writer.write(
-                "<html>\n" +
-                        "<head>\n" +
-                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n" +
-                        "    <link rel=\"stylesheet\" href=\"css/style.css\">\n" +
-                        "    <title>Список всех резюме</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "<section>\n" +
-                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">\n" +
-                        "    <tr>\n" +
-                        "        <th>Имя</th>\n" +
-                        "        <th>Email</th>\n" +
-                        "    </tr>\n");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr>\n" +
-                            "     <td><a href=\"resume?uuid=" + resume.getUuid() + "\">" + resume.getFullName() + "</a></td>\n" +
-                            "     <td>" + resume.getContact(ContactType.MAIL) + "</td>\n" +
-                            "</tr>\n");
-        }
-        writer.write("</table>\n" +
-                "</section>\n" +
-                "</body>\n" +
-                "</html>\n");
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
+
 }
