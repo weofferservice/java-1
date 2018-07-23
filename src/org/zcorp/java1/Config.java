db4fe6b53e1ccdffeb3c4276c243aa41686727a2
weofferservice <weofferservice@hotmail.com>
2018-07-23 15:25:11 +0300

    Подготовка приложения для деплоя в Heroku (вычитка ресурсов из CLASSPATH-а и задание креденшиалов к БД Heroku)

--- src/org/zcorp/java1/Config.java
+++ src/org/zcorp/java1/Config.java
@@ -4,13 +4,12 @@ import org.zcorp.java1.storage.SqlStorage;
 import org.zcorp.java1.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
+    private static final String PROPS = "/resumes.properties";
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -22,10 +21,10 @@ public class Config {
 
     private Config() {
         Properties props = new Properties();
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = getClass().getResourceAsStream(PROPS)) {
             props.load(is);
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath(), e);
+            throw new IllegalStateException("Invalid config file " + PROPS, e);
         }
         storageDir = new File(props.getProperty("storage.dir"));
         storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
@@ -37,14 +36,5 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
-    }
-
-    private static File getHomeDir() {
-        String prop = System.getProperty("homeDir");
-        File homeDir = new File(prop == null ? "." : prop);
-        if (!homeDir.isDirectory()) {
-            throw new IllegalStateException(homeDir + " is not directory");
-        }
-        return homeDir;
     }
 }
